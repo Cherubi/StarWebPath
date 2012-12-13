1d5d233db0e16fd6699da310e74edd719f71bb62
Cherubi <jetuomin@cs.helsinki.fi>
2012-12-13 23:13:16 +0200

    projekti

---
+++ StarWebPath/src/starwebpath/StarWebPath.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package starwebpath;
+
+/**
+ *
+ * @author Cherubi
+ */
+public class StarWebPath {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
